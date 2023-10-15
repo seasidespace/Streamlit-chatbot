65b16d91777556bf50fc4e89072eb3c1a145fa6d
seasidespace <144977810+seasidespace@users.noreply.github.com>
2023-10-15 20:03:02 +0000

    snowflake connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,34 +2,13 @@ import streamlit as st
 import os
 import openai
 
-st.title("☃️ Frosty")
 
-# Initialize the chat messages history
-if "messages" not in st.session_state.keys():
-    st.session_state.messages = [
-        {"role": "assistant", "content": "How can I help?"}
-    ]
+st.title('❄️ Welcome to Snowflake Business Intelligent Chatbot')
 
-# Prompt for user input and save
-if prompt := st.chat_input():
-    st.session_state.messages.append({"role": "user", "content": prompt})
+# Establish Snowflake session
+@st.cache_resource
+def create_session():
+    return Session.builder.configs(st.secrets.connections.snowpark).create()
 
-# display the existing chat messages
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.write(message["content"])
-
-# If last message is not from assistant, we need to generate a new response
-if st.session_state.messages[-1]["role"] != "assistant":
-    # Call LLM
-    with st.chat_message("assistant"):
-        with st.spinner("Thinking..."):
-            r = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages],
-            )
-            response = r.choices[0].message.content
-            st.write(response)
-
-    message = {"role": "assistant", "content": response}
-    st.session_state.messages.append(message)
+session = create_session()
+st.success("Connected to Snowflake!")
