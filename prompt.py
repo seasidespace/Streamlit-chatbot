b6571d5bf13322d91453d9a5b7a79d247db32bb4
seasidespace <144977810+seasidespace@users.noreply.github.com>
2023-10-16 02:23:10 +0000

    update

--- prompt.py
+++ prompt.py
@@ -44,8 +44,8 @@ Then provide 3 example questions using bullet points.
 
 @st.cache_data(show_spinner=True)
 def get_table_context(table_name: str, table_description: str, metadata_query: str = None):
+    table = table_name.split(".")    
     conn = st.experimental_connection("snowpark")
-    table = table_name.split(".")
     columns = conn.query(f"""
         SELECT COLUMN_NAME, DATA_TYPE FROM {table[0].upper()}.INFORMATION_SCHEMA.COLUMNS
         WHERE TABLE_SCHEMA = '{table[1].upper()}' AND TABLE_NAME = '{table[2].upper()}'
