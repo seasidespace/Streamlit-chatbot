adbd8bd184e0e676a5ad730ce4ab972b2c78b351
seasidespace <144977810+seasidespace@users.noreply.github.com>
2023-10-17 07:35:57 +0000

    big change!

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,9 @@
+"""
 import streamlit as st
 import os
 import openai
 from snowflake.snowpark import Session
-from prompt import get_system_prompt
 import re
-
-
 
 st.title('❄️Snowflake Business Intelligent Chatbot')
 
@@ -59,4 +57,132 @@ if st.session_state.messages[-1]["role"] != "assistant":
             message["results"] = conn.query(sql)
             st.dataframe(message["results"])
         st.session_state.messages.append(message)
+
+"""
+
+from langchain.agents.agent_toolkits import SQLDatabaseToolkit
+from langchain.utilities import SQLDatabase
+from langchain.llms import OpenAI
+from langchain_experimental.sql import SQLDatabaseSequentialChain
+from langchain.sql_database import SQLDatabase
+from langchain.agents import AgentExecutor
+from sqlalchemy.dialects import registry
+from snowflake.snowpark import Session
+import streamlit as st
+import openai
+import re
+import os
+
+
+# secrets to login snowflake db
+user = st.secrets.connections.snowpark.user
+password = st.secrets.connections.snowpark.password
+warehouse = st.secrets.connections.snowpark.warehouse
+role_name = st.secrets.connections.snowpark.role
+account = st.secrets.connections.snowpark.account
+database = st.secrets.connections.snowpark.database
+schema = st.secrets.connections.snowpark.schema
+
+# register the snowflake dialects
+registry.load("snowflake")
+registry.register('snowflake', 'snowflake.sqlalchemy', 'dialect')
+
+# Establish Snowflake session
+@st.cache_resource
+def create_session():
+    return Session.builder.configs(st.secrets.connections.snowpark).create()
+
+# Display the connection success message
+session = create_session()
+st.success("Connected to Snowflake!")
+
+# create snowflake db instance
+conn_string = f"snowflake://{user}:{password}@{account}/{database}/{schema}?warehouse={warehouse}&role={role_name}"
+db = SQLDatabase.from_uri(conn_string)
+
+# create the sql agent to communicate with the snowflake database
+agent_executor = SQLDatabaseSequentialChain.from_llm(
+    db = db,
+    llm=OpenAI(temperature=0),
+    verbose=True,
+    return_intermediate_steps=True
+    )
+
+INITIAL_MESSAGE = [
+    {"role": "user", "content": "Hi!"},
+    {
+        "role": "assistant",
+        "content": "Hey there, I'm Chatty McQueryFace, your SQL-speaking sidekick, ready to chat up Snowflake and fetch answers faster than a snowball fight in summer! ❄️🔍",
+    },
+]
+
+# Initialize the chat messages history
+if "messages" not in st.session_state.keys():
+    st.session_state["messages"] = INITIAL_MESSAGE
+
+if "history" not in st.session_state:
+    st.session_state["history"] = []
+
+if "model" not in st.session_state:
+    st.session_state["model"] = model
+
+# Prompt for user input and save
+if prompt := st.chat_input():
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+for message in st.session_state.messages:
+    message_func(
+        message["content"],
+        True if message["role"] == "user" else False,
+        True if message["role"] == "data" else False,
+    )
+
+def append_chat_history(question, answer):
+    st.session_state["history"].append((question, answer))
+
+def append_message(content, role="assistant", display=False):
+    message = {"role": role, "content": content}
+    st.session_state.messages.append(message)
+    if role != "data":
+        append_chat_history(st.session_state.messages[-2]["content"], content)
+
+    if callback_handler.has_streaming_ended:
+        callback_handler.has_streaming_ended = False
+        return
+response = agent_executor(prompt)
+
+if st.session_state.messages[-1]["role"] != "assistant":
+    content = st.session_state.messages[-1]["content"]
+    if isinstance(content, str):
+        result = chain(
+            {"question": content, "chat_history": st.session_state["history"]}
+        )["answer"]
+        print(result)
+        append_message(result)
+        if get_sql(result):
+            conn = SnowflakeConnection().get_session()
+            df = execute_sql(get_sql(result), conn)
+            if df is not None:
+                callback_handler.display_dataframe(df)
+                append_message(df, "data", True)
+
+
+"""
+print('sql query: ', response['intermediate_steps'][1])
+print('orginal question: ', response['query'])
+print('final answer: ', response['result'])
+print('sql result: ',response['intermediate_steps'][3])
+"""
+
+
+if prompt := st.chat_input():
+    st.chat_message("user").write(prompt)
+    with st.chat_message("assistant"):
+        response = agent_executor.run(prompt)
+        st.write( response['intermediate_steps'][1])
+        st.write(response['query'])
+        st.write(response['result'])
+        st.write(response['intermediate_steps'][3])
+
+
 
